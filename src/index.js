d4bc5a45b47b08edcd8afbd2537a83d7f5a6ab61
Mike Davlantes <rocks722@hotmail.com>
2022-12-06 03:14:33 -0700

    added funky favicons to change with the map

--- src/index.js
+++ src/index.js
@@ -11,15 +11,17 @@ let map = new maplibregl.Map({
 })
 
 let discoColors = [
-  '#ff0000',
-  '#ffaa00',
-  '#aaff00',
-  '#00ff00',
-  '#00ffa9',
-  '#00a9ff',
-  '#0000ff',
-  '#aa00ff',
-  '#ff00aa'
+  // colors and their associated png favicon. it's dumb that parceljs can't handle static assets better and makes me import them...
+  // well in 30 minutes of struggling nothing worked and i got impatient, so maybe if you dig deeper they're a way.
+  ['#ff0000', require('./funky-favicons/favicon-ff0000.png')],
+  ['#ffaa00', require('./funky-favicons/favicon-ffaa00.png')],
+  ['#aaff00', require('./funky-favicons/favicon-aaff00.png')],
+  ['#00ff00', require('./funky-favicons/favicon-00ff00.png')],
+  ['#00ffa9', require('./funky-favicons/favicon-00ffa9.png')],
+  ['#00a9ff', require('./funky-favicons/favicon-00a9ff.png')],
+  ['#0000ff', require('./funky-favicons/favicon-0000ff.png')],
+  ['#aa00ff', require('./funky-favicons/favicon-aa00ff.png')],
+  ['#ff00aa', require('./funky-favicons/favicon-ff00aa.png')]
 ]
 
 function shuffle(array) {
@@ -43,10 +45,15 @@ function getFunky() {
   let fillColors = shuffle([3, 5, 7, 9, 11, 13, 15, 17, 18])
 
   fillColors.forEach((fillColorIndex, arrayIndex) => {
-    mapStyle.layers[1].paint['fill-color'][fillColorIndex] = discoColors[arrayIndex]
+    mapStyle.layers[1].paint['fill-color'][fillColorIndex] = discoColors[arrayIndex][0]
   })
 
   map.setStyle(mapStyle)
+
+  // set the favicon to a random color
+  let randomColorFavicon = discoColors[Math.floor(Math.random() * discoColors.length)][1]
+  let favicon = document.querySelector("link[rel~='icon']")
+  favicon.href = randomColorFavicon
 }
 
 document.getElementById('party-button').onclick = () => {
